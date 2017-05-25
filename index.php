94c61ec02c6fe2d7c7d917591d9b175c64ed0e24
danimvijay <danimvijay@gmail.com>
2017-05-25 17:36:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="#">CRM v2</a> </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">SGU</a></li>
+                            <li><a href="#">OPF</a></li>
+                            <li><button class="btn btn-success navbar-btn">Phone Inquiry</button></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">SGU</a></li>
+                    <li><a href="#">OPF</a></li>
+                    <li><button class="btn btn-success navbar-btn" style="margin-left:5px;">Phone Inquiry</button></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-footer">Panel Footer</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
